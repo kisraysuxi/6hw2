c903f98b60d23dab521fd2bb3f6d2a5adda5b454
kisraysuxi <elinatadu@icloud.com>
2023-03-02 19:29:18 +0600

    hw

--- 6hw2/main.swift
+++ 6hw2/main.swift
@@ -1,11 +1,29 @@
-//
-//  main.swift
-//  6hw2
-//
-//  Created by Pontophone on 2/24/23.
-//
+//Домашнее задание №6. Замыкания
+//1. Составить замыкание, которое считает кол-во букв в нашем предложении, которое мы вводим в консоли.
+//print("Ваш текст:")
+//var text = readLine()!
+//var sentence: (String) -> Int
+//sentence = { (sentence: String) -> Int in return text.count }
+//var res = sentence(text)
+//print("В вашем тексте \(res) букв")
 
-import Foundation
+//2. Составить замыкание, которое переводит наши деньги в курс доллара.
+//var som = 80
+//print("dollars:")
+//var dollar = Int(readLine()!)!
+//var transaction: (Int) -> Int
+//transaction = {(money: Int) -> Int in return dollar * som}
+//var res1 = transaction(dollar)
+//print("\(som) переведены в \(res1)")
+//3. Составить программу с помощью функции sorted(), которая cортирует список имен по кол-ву букв по возрастанию.
+//var names = ["elina", "sasha", "artur", "dana"]
+//names.sorted(by: { names, names in true })
+//print(names)
 
-print("Hello, World!")
+
+
+
+
+
+
 
